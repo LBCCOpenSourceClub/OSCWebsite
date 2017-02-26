0fd03831d55aa8216221a8ea3ee1ec75a993873b
grantmerola <grantmerola@gmail.com>
2017-02-25 22:44:31 -0800

    add projects page changed side bar to reflect changes

--- buildosc.sh
+++ buildosc.sh
@@ -4,3 +4,5 @@ open -a "Google Chrome" index.html
 slimrb osc.slim > index_min.html
 echo "index_min size"
 wc -c index_min.html
+slimrb -p projects.slim > projects.html
+slimrb projects.slim > projects_min.html
